76f4cc530903fb4e860f4ba71cd7f706147f7876
jack.kuo <dragon_kuo@hotmail.com>
2012-08-21 21:41:07 +0800

    first version

---
+++ src/btcon/btcubs/BtconActivity.java
@@ -0,0 +1,33 @@
+package btcon.btcubs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BtconActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    public void Discovery()
+    {
+    	
+    }
+    public void Search()
+    {   	
+    
+    }
+    public void Connect()
+    {
+    
+    }
+    public void SendCmd()
+    {
+    	
+    }
+    private void RegEvent()
+    {
+    	
+    }
+}
